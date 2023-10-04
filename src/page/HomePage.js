c01b3b90cd1bff4c6143846055c3397856c5815c
Khalil <Khalil.haouas@gmail.com>
2023-10-04 15:01:46 +0200

    endcommit

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -2,14 +2,16 @@ import Header from "../components/Header";
 import inputSearch from "../components/inputSearch";
 import Pagination from "../components/pagination";
 import PersonList from "../components/personneListe";
-import {fetchPersons} from "../Tools/fetchPersonne"
+import { fetchPersons } from "../Tools/fetchPersonne";
 
 const App = () => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const name = searchParams.get('name') ?? '';
-    const page = searchParams.get('page')?? '';
-    fetchPersons(name, page);
-    return /*html */`
+  const path = window.location.pathname.slice(1);
+  console.log("path",path);
+  const searchParams = new URLSearchParams(window.location.search);
+  const name = searchParams.get("name") ?? "";
+  const page = searchParams.get("page") ?? "";
+  fetchPersons(name, page);
+  return /*html */ `
         <div class=" container py-10">
         ${Header()}
         ${inputSearch(name)}
@@ -17,6 +19,6 @@ const App = () => {
         ${Pagination()}
         </div>
     `;
-}
+};
 
 export default App;
