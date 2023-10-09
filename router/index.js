9bed771b652af0f418ecb6f5a6a11def04b6d229
Khalil <Khalil.haouas@gmail.com>
2023-10-09 20:05:54 +0200

    finisched

--- router/index.js
+++ router/index.js
@@ -19,7 +19,7 @@ const Router = () => {
         if (route.length > 1) {
           return episode(route[1]);
         }
-        return EpisodePage
+        return EpisodePage ()
   }
   return HomePage();
 };
